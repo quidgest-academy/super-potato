75b64327282242e0b5e7991e776ef4a92b1ea322
andre.seco <andre.seco@users.noreply.quidgest.pt>
2026-04-23 12:38:36 +0100

    Build version: 73 | Genio version: 382.64 | Database version: 2530 | Platform: VUE

--- GEN_QUIDGEST/MYAPP/ClientApp/apps/geniovue/src/plugins/quidgest-ui.js
+++ GEN_QUIDGEST/MYAPP/ClientApp/apps/geniovue/src/plugins/quidgest-ui.js
@@ -18,7 +18,7 @@ const framework = createFramework({
 	},
 	defaults: {
 		QIconSvg: {
-			bundle: 'Content/svgbundle.svg?v=72'
+			bundle: 'Content/svgbundle.svg?v=73'
 		},
 		QCollapsible: {
 			icons: {
