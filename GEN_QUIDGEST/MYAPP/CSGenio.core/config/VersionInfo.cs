0f85d4698b5b26be0ba317463d7b9e14a86160b5
andre.seco <andre.seco@users.noreply.quidgest.pt>
2026-04-27 10:06:32 +0100

    Build version: 106 | Genio version: 382.64 | Database version: 2537 | Platform: VUE

--- GEN_QUIDGEST/MYAPP/CSGenio.core/config/VersionInfo.cs
+++ GEN_QUIDGEST/MYAPP/CSGenio.core/config/VersionInfo.cs
@@ -45,7 +45,7 @@ namespace CSGenio.framework
         /// <summary>
         /// Generated version. Is incremented each time there is a generation
         /// </summary>
-        public const int Generation = 105;
+        public const int Generation = 106;
 
 
         /// <summary>
@@ -93,7 +93,7 @@ namespace CSGenio.framework
         /// <summary>
         /// Date when the code was generated
         /// </summary>
-        public static readonly DateTime GenerationDate = new DateTime(2026, 4, 24);
+        public static readonly DateTime GenerationDate = new DateTime(2026, 4, 27);
 
     }
 }
--- GEN_QUIDGEST/MYAPP/ClientApp/apps/geniovue/src/plugins/quidgest-ui.js
+++ GEN_QUIDGEST/MYAPP/ClientApp/apps/geniovue/src/plugins/quidgest-ui.js
@@ -18,7 +18,7 @@ const framework = createFramework({
 	},
 	defaults: {
 		QIconSvg: {
-			bundle: 'Content/svgbundle.svg?v=105'
+			bundle: 'Content/svgbundle.svg?v=106'
 		},
 		QCollapsible: {
 			icons: {
